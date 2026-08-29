d90bd8995559b197c2206603c0c441b9f51e6237
Akash Bijwe <mr.akashbijwe@gmail.com>
2026-08-29 15:16:35 +0530

    Add LED indicator and adjust timer duration
    Added LED functionality to indicate object detection and changed timer duration from 5 seconds to 3 seconds.

--- projects/HC-SR04 + servo.ino
+++ projects/HC-SR04 + servo.ino
@@ -5,6 +5,7 @@ Servo myServo;
 const int SERVO_PIN = 7;
 const int TRIGGER_PIN = 3; // Input to UNO
 const int ECHO_PIN = 2; // Output to UNO
+const int LED_PIN = 5;
 
 bool objectDetected = false;
 unsigned long objectLeftTime = 0;
@@ -14,6 +15,7 @@ void setup() {
 
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 
   myServo.attach(SERVO_PIN);
   myServo.write(0);
@@ -21,7 +23,7 @@ void setup() {
 
 void loop() {
   long duration, inches;
-
+	
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, HIGH);
@@ -38,6 +40,7 @@ void loop() {
   if (inches < 20) {
     if (!objectDetected) {
       objectDetected = true;
+      digitalWrite(LED_PIN, HIGH);
       myServo.write(180);
       Serial.println("Object detected -> Servo 180°");
     }
@@ -45,11 +48,12 @@ void loop() {
     if (objectDetected) {
       objectDetected = false;
       objectLeftTime = millis();
-      Serial.println("Object left -> Starting 5 second timer");
+      Serial.println("Object left -> Starting 3 second timer");
     }
-    if (millis() - objectLeftTime >= 5000) {
+    if (millis() - objectLeftTime >= 3000) {
       myServo.write(0);
-      Serial.println("5 seconds passed -> Servo 0°");
+      Serial.println("3 seconds passed -> Servo 0°");
+      digitalWrite(LED_PIN, LOW);
     }
   }
 }
